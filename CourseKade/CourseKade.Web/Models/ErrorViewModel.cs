bf8c388369cd5416aa18cf01f8036fefd7c29cea
Yoones Asgarian <yoonesasgasg@gmail.com>
2022-10-18 17:56:57 +0330

    create web project
    create web project

---
+++ CourseKade/CourseKade.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CourseKade.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
